0e26f4cbab215815a4d1ded26c86e9b3baf26c88
Michael Choi <michael@Michaels-MacBook-Pro.local>
2018-04-28 11:54:12 -0700

    changed the name

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -7,12 +7,12 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
+    print(request.form)
     return render_template("checkout.html")
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
-
 
 if __name__=="__main__":   
     app.run(debug=True)    
